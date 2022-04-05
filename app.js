03a1768c6f5ed9ecdf78fd3f70b1f8bbb5ac8caa
Sofiane M <poubellepropre8@gmail.com>
2022-04-05 20:19:05 +0200

    well advanced lab

--- app.js
+++ app.js
@@ -5,14 +5,55 @@ const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
 
+const SpotifyWebApi = require('spotify-web-api-node');
+
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (request, response) => {
+  response.render('home');
+});
+
+app.get('/artist-search', (request, response) => {
+    const term = request.query.term;
+    spotifyApi
+  .searchArtists(term)
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    const artists = data.body.artists.items;
+    response.render('artist-search-results', {artists}  );
+    
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+  });
+
+
+  app.get('/albums/:artistID', (request, response) => {
+    const artistID = request.params.artistID;
+    
+  });
+
+
 // setting the spotify-api goes here:
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch((error) =>
+    console.log('Something went wrong when retrieving an access token', error)
+  );
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.listen(3000, () =>
+  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
+);
